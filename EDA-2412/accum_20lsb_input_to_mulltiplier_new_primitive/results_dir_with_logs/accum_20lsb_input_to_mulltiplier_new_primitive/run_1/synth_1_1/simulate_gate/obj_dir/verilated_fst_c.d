8a1d66d39affbf5d4b5374072f06fefb63ae7ae0
abdulhameed-akram <aahameedd@gmail.com>
2024-01-23 14:01:15 +0500

    dsp test update

---
+++ EDA-2412/accum_20lsb_input_to_mulltiplier_new_primitive/results_dir_with_logs/accum_20lsb_input_to_mulltiplier_new_primitive/run_1/synth_1_1/simulate_gate/obj_dir/verilated_fst_c.d
@@ -0,0 +1,22 @@
+verilated_fst_c.o: \
+ /nfs_eda_sw/softwares/Raptor/instl_dir/01_22_2024_09_15_01/bin/HDL_simulator/verilator/include/verilated_fst_c.cpp \
+ /nfs_eda_sw/softwares/Raptor/instl_dir/01_22_2024_09_15_01/bin/HDL_simulator/verilator/include/verilated.h \
+ /nfs_eda_sw/softwares/Raptor/instl_dir/01_22_2024_09_15_01/bin/HDL_simulator/verilator/include/verilatedos.h \
+ /nfs_eda_sw/softwares/Raptor/instl_dir/01_22_2024_09_15_01/bin/HDL_simulator/verilator/include/verilated_config.h \
+ /nfs_eda_sw/softwares/Raptor/instl_dir/01_22_2024_09_15_01/bin/HDL_simulator/verilator/include/verilated_types.h \
+ /nfs_eda_sw/softwares/Raptor/instl_dir/01_22_2024_09_15_01/bin/HDL_simulator/verilator/include/verilated_funcs.h \
+ /nfs_eda_sw/softwares/Raptor/instl_dir/01_22_2024_09_15_01/bin/HDL_simulator/verilator/include/verilated_fst_c.h \
+ /nfs_eda_sw/softwares/Raptor/instl_dir/01_22_2024_09_15_01/bin/HDL_simulator/verilator/include/verilated_trace.h \
+ /nfs_eda_sw/softwares/Raptor/instl_dir/01_22_2024_09_15_01/bin/HDL_simulator/verilator/include/verilated_trace_defs.h \
+ /nfs_eda_sw/softwares/Raptor/instl_dir/01_22_2024_09_15_01/bin/HDL_simulator/verilator/include/gtkwave/fstapi.h \
+ /nfs_eda_sw/softwares/Raptor/instl_dir/01_22_2024_09_15_01/bin/HDL_simulator/verilator/include/gtkwave/fastlz.c \
+ /nfs_eda_sw/softwares/Raptor/instl_dir/01_22_2024_09_15_01/bin/HDL_simulator/verilator/include/gtkwave/fastlz.h \
+ /nfs_eda_sw/softwares/Raptor/instl_dir/01_22_2024_09_15_01/bin/HDL_simulator/verilator/include/gtkwave/fastlz.c \
+ /nfs_eda_sw/softwares/Raptor/instl_dir/01_22_2024_09_15_01/bin/HDL_simulator/verilator/include/gtkwave/fstapi.c \
+ /nfs_eda_sw/softwares/Raptor/instl_dir/01_22_2024_09_15_01/bin/HDL_simulator/verilator/include/gtkwave/fst_config.h \
+ /nfs_eda_sw/softwares/Raptor/instl_dir/01_22_2024_09_15_01/bin/HDL_simulator/verilator/include/gtkwave/fstapi.h \
+ /nfs_eda_sw/softwares/Raptor/instl_dir/01_22_2024_09_15_01/bin/HDL_simulator/verilator/include/gtkwave/lz4.h \
+ /nfs_eda_sw/softwares/Raptor/instl_dir/01_22_2024_09_15_01/bin/HDL_simulator/verilator/include/gtkwave/lz4.c \
+ /nfs_eda_sw/softwares/Raptor/instl_dir/01_22_2024_09_15_01/bin/HDL_simulator/verilator/include/verilated_trace_imp.h \
+ /nfs_eda_sw/softwares/Raptor/instl_dir/01_22_2024_09_15_01/bin/HDL_simulator/verilator/include/verilated_intrinsics.h \
+ /nfs_eda_sw/softwares/Raptor/instl_dir/01_22_2024_09_15_01/bin/HDL_simulator/verilator/include/verilated_threads.h
