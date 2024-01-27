394a2b191e1f36530d271cf7c17560437c61f3c0
alaindargelas <alain.dargelas@gmail.com>
2024-01-27 10:12:25 -0800

    fix test

--- EDA-1772/raptor_tcl.tcl
+++ EDA-1772/raptor_tcl.tcl
@@ -3,6 +3,7 @@ target_device GEMINI_COMPACT_104x68
 add_include_path ./rtl
 add_library_path ./rtl
 add_library_ext .v .sv
+add_design_file ./rtl/uart_defines.vh
 add_design_file ./rtl/vex_soc.v
 set_top_module vex_soc
 analyze
