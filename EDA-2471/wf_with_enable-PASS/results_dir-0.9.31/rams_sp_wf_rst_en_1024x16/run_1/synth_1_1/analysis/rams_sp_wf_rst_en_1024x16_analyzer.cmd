a5a24c190f36ebf700765638393306b2563c4101
abdulhameed-akram <aahameedd@gmail.com>
2024-02-03 16:33:20 +0500

    sim failure

---
+++ EDA-2471/wf_with_enable-PASS/results_dir-0.9.31/rams_sp_wf_rst_en_1024x16/run_1/synth_1_1/analysis/rams_sp_wf_rst_en_1024x16_analyzer.cmd
@@ -0,0 +1,5 @@
+read_verilog -sv /nfs_eda_sw/softwares/Raptor/instl_dir/02_03_2024_09_15_01/share/raptor/sim_models/rapidsilicon/genesis3/FPGA_PRIMITIVES_MODELS/blackbox_models/cell_sim_blackbox.v
+verilog_defines 
+read_verilog  -I../../../../.././rtl -I../../../../.. -I/nfs_scratch/scratch/CompilerValidation/abdul_hameed/Validation/RTL_testcases/Memory_Designs/rams_sp_wf_rst_1024x16/EDA-2471/wf_with_enable-PASS/results_dir/.././rtl  /nfs_scratch/scratch/CompilerValidation/abdul_hameed/Validation/RTL_testcases/Memory_Designs/rams_sp_wf_rst_1024x16/EDA-2471/wf_with_enable-PASS/results_dir/.././rtl/rams_sp_wf_rst_en_1024x16.v 
+
+analyze -top rams_sp_wf_rst_en_1024x16
