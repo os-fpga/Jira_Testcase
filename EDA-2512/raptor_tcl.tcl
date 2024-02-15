8f3748a0b5e27bf4afcaf4bd4d028cd70b5f6f0d
sarmadsalman-RS <sarmad.salman@rapidsilicon.com>
2024-02-15 12:50:16 +0500

    corrections made for testing

--- EDA-2512/raptor_tcl.tcl
+++ EDA-2512/raptor_tcl.tcl
@@ -1,8 +1,8 @@
 create_design axil_interconnect_wrapper
 target_device GEMINI_COMPACT_104x68
-add_include_path ../rtl
-add_library_path ../rtl
+add_include_path ./rtl
+add_library_path ./rtl
 add_library_ext .v .sv
-add_design_file ../rtl/axil_interconnect_wrapper.v
+add_design_file ./rtl/axil_interconnect_wrapper_v1_0.v
 set_top_module axil_interconnect_wrapper
 synthesize
