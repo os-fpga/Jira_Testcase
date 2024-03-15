c1ddd8e86fa5942a3ceac06a4a55cf15d4c03265
alaindargelas <alain.dargelas@gmail.com>
2024-03-14 22:46:33 -0700

    Turn on partitionner

--- EDA-2355/raptor.tcl
+++ EDA-2355/raptor.tcl
@@ -1,5 +1,22 @@
 create_design eh2_dec_02_24
 target_device GEMINI_COMPACT_104x68
+
+
+# Create or open vpr_constraints.xml file for writing
+set filename "vpr_constraints.xml"
+set fileId [open $filename "w"]
+
+# XML content to write
+set xml_content {<vpr_constraints tool_name="vpr">
+</vpr_constraints>}
+
+# Write the XML content to the file
+puts $fileId $xml_content
+
+# Close the file
+close $fileId
+
+
 add_include_path ./rtl
 add_library_path ./rtl
 add_library_ext .v .sv
@@ -15,10 +32,13 @@ add_design_file ./rtl/eh2_dec_tlu_ctl.sv
 add_design_file ./rtl/eh2_dec_tlu_top.sv
 add_design_file ./rtl/eh2_dec_trigger.sv
 add_design_file ./rtl/eh2_dec.sv
+add_constraint_file constraints.sdc
 set_top_module eh2_dec
 pin_loc_assign_method free
 analyze
-synthesize 
+synthesize
+pnr_options --use_partitioning_in_pack on --number_of_molecules_in_partition 16 --read_vpr_constraints vpr_constraints.xml
+
 packing
 global_placement
 place
