74f80f48c6b110fe5d0f71c997a12c1430852cff
zhergarvi <zhergarvi133@gmail.com>
2024-03-21 15:11:45 +0500

    added the and8 testcase with scripts

---
+++ EDA-2429/and2/and8/sim/bitstream_tb/and8_include_netlists.v
@@ -0,0 +1,6 @@
+`timescale 1ns / 1ps
+
+`include "../../../SRC/fabric_netlists.v"
+
+// `include "./BIT_SIM/and8_top_formal_verification.v"
+`include "../../sim/bitstream_tb/and8_formal_random_top_tb.v"
