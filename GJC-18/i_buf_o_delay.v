1a9ad35bc310671ef88e237aa9b4acac178049e0
valence-e <azfartariq554@gmail.com>
2024-03-25 23:05:01 +0500

    added io use cases for i_ and o_ delay

---
+++ GJC-18/i_buf_o_delay.v
@@ -0,0 +1,42 @@
+//***********************************************************
+// Functionality: Alternating 10 generated is delayed and sent out,
+//                also a non delayed version of same data is sent
+//                out for reference. Using inverted control signals,
+//                delay can be adjusted accordingly
+// Author:        Azfar  
+//***********************************************************
+
+
+
+module i_buf_o_delay (
+    input wire clk_i,
+    input wire dly_incdec,
+    input wire dly_ld,
+    input wire dly_adj,
+    output wire data_o_delayed,
+    output wire data_o_ref,
+    output wire [5:0] dly_tap_val_inv
+);
+
+    reg test_data;
+    wire dly_adj_inv, dly_incdec_inv, dly_ld_inv;
+    wire [5:0] dly_tap_val;
+
+    always @(clk_i) begin
+        if(clk_i)test_data <= 1;
+        else test_data <= 0;
+    end
+    assign data_o_ref       = test_data;
+    assign dly_adj_inv      = ~dly_adj;
+    assign dly_incdec_inv   = ~dly_incdec;
+    assign dly_ld_inv       = ~dly_ld;
+    assign dly_tap_val_inv  = ~dly_tap_val;
+
+    O_DELAY data_o_delay (  .I(test_data), 
+                            .DLY_LOAD(dly_ld_inv), 
+                            .DLY_ADJ(dly_adj_inv), 
+                            .DLY_INCDEC(dly_incdec_inv), 
+                            .CLK_IN(clk_i), 
+                            .DLY_TAP_VALUE(dly_tap_val),
+                            .O(data_o_delayed));
+endmodule
---
+++ GJC-19/i_delay_o_buf.v
@@ -0,0 +1,37 @@
+//***********************************************************
+// Functionality: Input is delayed according to controls then 
+//                inverted and sent out through output buffer
+// Author:        Azfar  
+//***********************************************************
+
+
+
+module i_delay_o_buf (
+    input wire clk_i,
+    input wire data_i,
+    input wire dly_incdec,
+    input wire dly_ld,
+    input wire dly_adj,
+    output wire data_o_inv_delayed,
+    output wire [5:0] dly_tap_val_inv
+);
+
+    wire data_o;
+    wire [5:0] dly_tap_val;
+    wire dly_incdec_inv, dly_adj_inv, dly_ld_inv;
+
+    assign data_o_inv_delayed   = ~data_o;
+
+    assign dly_incdec_inv       = ~dly_incdec;
+    assign dly_adj_inv          = ~dly_adj;
+    assign dly_ld_inv           = ~dly_ld;
+    assign dly_tap_val_inv      = ~dly_tap_val;
+
+    I_DELAY data_i_delay (  .I(data_i),
+                            .DLY_LOAD(dly_ld_inv),
+                            .DLY_ADJ(dly_adj_inv),
+                            .DLY_INCDEC(dly_incdec_inv),
+                            .DLY_TAP_VALUE(dly_tap_val),
+                            .CLK_IN(clk_i),
+                            .O(data_o));
+endmodule
