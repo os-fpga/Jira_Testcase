83008ca6f02d10fa7bc255c4d159e4243d056d0b
valence-e <azfartariq554@gmail.com>
2024-04-08 11:36:20 +0500

    added designs that consumes pll

--- GJC-20/raptor.tcl
+++ GJC-20/raptor.tcl
@@ -1,6 +1,6 @@
-create_design i_buf_i_delay_i_ddr_to_sdr
-add_design_file i_buf_i_delay_i_ddr_to_sdr.v
-set_top_module i_buf_i_delay_i_ddr_to_sdr
+create_design i_buf_i_delay_i_ddr_clk_buf
+add_design_file i_buf_i_delay_i_ddr_clk_buf.v
+set_top_module i_buf_i_delay_i_ddr_clk_buf
 add_constraint_file constraints.sdc
 add_constraint_file pin_constraints.pin
 target_device 1VG28
