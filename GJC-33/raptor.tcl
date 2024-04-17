94101be43cf939af4f19b4d05e94a698264444e0
valence-e <azfartariq554@gmail.com>
2024-04-17 12:33:20 +0500

    added designs with different pll usage

---
+++ GJC-33/raptor.tcl
@@ -0,0 +1,14 @@
+create_design boot_clk_pll
+add_design_file boot_clk_pll.v
+set_top_module boot_clk_pll
+add_constraint_file constraints.sdc
+add_constraint_file pin_constraints.pin
+target_device 1VG28
+
+analyze
+synthesize
+packing
+place
+route
+sta
+bitstream
