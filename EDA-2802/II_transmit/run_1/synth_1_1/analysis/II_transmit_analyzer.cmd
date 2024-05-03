e56acc4f2bf3fa2fdf133aad2bcfbe763b5ad33e
bbambhania-pub <b.d.bambhania@gmail.com>
2024-05-03 19:32:36 +0500

    add jira EDA-2802

---
+++ EDA-2802/II_transmit/run_1/synth_1_1/analysis/II_transmit_analyzer.cmd
@@ -0,0 +1,8 @@
+read_verilog -sv /nfs_eda_sw/softwares/Raptor/instl_dir/04_30_2024_09_15_01/share/raptor/sim_models/rapidsilicon/genesis3/FPGA_PRIMITIVES_MODELS/blackbox_models/cell_sim_blackbox.v
+verilog_defines 
+read_verilog -sv -I../../../../.././rtl -I../../../../.. -I/nfs_scratch/scratch/CGA/repo/2024-04-30-11-45-48_T10763R122/Validation/RTL_testcases/RTL_Benchmarks_Gap_Analysis/II_transmit/results_dir/.././rtl  /nfs_scratch/scratch/CGA/repo/2024-04-30-11-45-48_T10763R122/Validation/RTL_testcases/RTL_Benchmarks_Gap_Analysis/II_transmit/results_dir/.././rtl/ll_tx_cred.sv 
+read_verilog -sv -I../../../../.././rtl -I../../../../.. -I/nfs_scratch/scratch/CGA/repo/2024-04-30-11-45-48_T10763R122/Validation/RTL_testcases/RTL_Benchmarks_Gap_Analysis/II_transmit/results_dir/.././rtl  /nfs_scratch/scratch/CGA/repo/2024-04-30-11-45-48_T10763R122/Validation/RTL_testcases/RTL_Benchmarks_Gap_Analysis/II_transmit/results_dir/.././rtl/ll_tx_ctrl.sv 
+read_verilog -sv -I../../../../.././rtl -I../../../../.. -I/nfs_scratch/scratch/CGA/repo/2024-04-30-11-45-48_T10763R122/Validation/RTL_testcases/RTL_Benchmarks_Gap_Analysis/II_transmit/results_dir/.././rtl  /nfs_scratch/scratch/CGA/repo/2024-04-30-11-45-48_T10763R122/Validation/RTL_testcases/RTL_Benchmarks_Gap_Analysis/II_transmit/results_dir/.././rtl/syncfifo_reg.sv 
+read_verilog -sv -I../../../../.././rtl -I../../../../.. -I/nfs_scratch/scratch/CGA/repo/2024-04-30-11-45-48_T10763R122/Validation/RTL_testcases/RTL_Benchmarks_Gap_Analysis/II_transmit/results_dir/.././rtl  /nfs_scratch/scratch/CGA/repo/2024-04-30-11-45-48_T10763R122/Validation/RTL_testcases/RTL_Benchmarks_Gap_Analysis/II_transmit/results_dir/.././rtl/ll_transmit.sv 
+
+analyze -top ll_transmit
