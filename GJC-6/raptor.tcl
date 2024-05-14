5ee7f4586139c3723049476e89c3103f9f5e9058
valence-e <azfartariq554@gmail.com>
2024-05-14 10:21:23 +0500

    changed device names and clock pins in GJC designs

--- GJC-6/raptor.tcl
+++ GJC-6/raptor.tcl
@@ -1,5 +1,5 @@
 create_design tristate
-target_device 1GE100-ES1
+target_device 1VG28
 add_design_file tristate.v
 add_constraint_file tristate.sdc
 ipgenerate
--- GJC-9/raptor.tcl
+++ GJC-9/raptor.tcl
@@ -1,5 +1,5 @@
 create_design ft
-target_device 1GVTC
+target_device 1VG28
 add_design_file ft.v
 add_constraint_file ft.sdc
 ipgenerate
