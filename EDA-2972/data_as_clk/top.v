3010d71f207ff19f14b64c5915f2c9c8ba5e1811
chungshien-chai <chungshien.chai@gmail.com>
2024-06-13 20:08:19 -0700

    Better naming

--- EDA-2972/data_as_clk/top.v
+++ EDA-2972/data_as_clk/top.v
@@ -9,24 +9,24 @@ module top
   // https://github.com/os-fpga/Jira_Testcase/tree/main/EDA-2969
   
   localparam FABRIC_CLK_GEN_METHOD = 1;
-  reg [1:0] fabric_clk;
+  reg [1:0] fabric_data_as_clk;
   generate
     if (FABRIC_CLK_GEN_METHOD == 0) begin
       // Method 0 generate clock via counter
       // This does not trigger assertion
       always @(posedge clk) begin
-        fabric_clk <= fabric_clk + 2'd1;
+        fabric_data_as_clk <= fabric_data_as_clk + 2'd1;
       end
     end
     else begin
       // Method none-0 generate clock via one-hot circular shifting
       // This trigger assertion
       always @(posedge clk) begin
-        if (fabric_clk == 2'd0 || fabric_clk == 2'd3)
-          fabric_clk <= 2'd1;
+        if (fabric_data_as_clk == 2'd0 || fabric_data_as_clk == 2'd3)
+          fabric_data_as_clk <= 2'd1;
         else begin
-          fabric_clk[1] <= fabric_clk[0];
-          fabric_clk[0] <= fabric_clk[1];
+          fabric_data_as_clk[1] <= fabric_data_as_clk[0];
+          fabric_data_as_clk[0] <= fabric_data_as_clk[1];
         end
       end
     end
@@ -35,7 +35,7 @@ module top
   generate
     for (i = 0; i < 2; i++) begin
       // Use generated fabric data as clock
-      always @(posedge fabric_clk[i]) begin
+      always @(posedge fabric_data_as_clk[i]) begin
         dout[i] <= din[i];
       end
     end
