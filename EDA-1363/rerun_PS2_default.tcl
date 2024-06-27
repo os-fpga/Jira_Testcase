358de175f38a6eb776f53a9fcb02cfe2a807fea9
alaindargelas <alain.dargelas@gmail.com>
2024-06-26 21:51:19 -0700

    Testcase for EDA-1363, Misleading error message

---
+++ EDA-1363/rerun_PS2_default.tcl
@@ -0,0 +1,13 @@
+create_design raptor_project_default_rerun
+target_device 1VG28
+add_design_file ./Src/timescale.v
+add_design_file ./Src/ps2_io_ctrl.v
+add_design_file ./Src/ps2_keyboard.v
+add_design_file ./Src/ps2_defines.v
+add_design_file ./Src/ps2_translation_table.v
+add_design_file ./Src/ps2_wb_if.v
+add_design_file ./Src/ps2_top.v
+add_constraint_file raptor_project_default/constraints.sdc
+analyze
+synthesize 
+packing
