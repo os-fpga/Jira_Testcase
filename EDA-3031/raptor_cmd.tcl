02f37429ad07c4abe709d2f3f347983d3fde6e9b
Abdul Hameed <45313849+abdulhameed-akram@users.noreply.github.com>
2024-07-08 15:06:56 +0500

    EDA-3031 (#232)

---
+++ EDA-3031/raptor_cmd.tcl
@@ -0,0 +1,22 @@
+# /*******************************************************************************
+# Copyright (c) 2022-2024 Rapid Silicon
+# This source code contains proprietary information belonging to Rapid Silicon
+# (the "licensor") released under license and non-disclosure agreement to the
+# recipient (the "licensee").
+# The information shared and protected by the license and non-disclosure agreement
+# includes but is not limited to the following:
+# * operational algorithms of the product
+# * logos, graphics, source code, and visual presentation of the product
+# * confidential operational information of the licensor
+# The recipient of this source code is NOT permitted to publicly disclose,
+# re-use, archive beyond the period of the license agreement, transfer to a
+# sub-licensee, or re-implement any portion of the content covered by the license
+# and non-disclosure agreement without the prior written consent of the licensor.
+# *********************************************************************************/
+# Version  : 2024.07
+# Build    : 1.1.23
+# Hash     : 55e3ad8
+# Date     : Jul  7 2024
+# Type     : Engineering
+# Log Time   : Mon Jul  8 09:51:57 2024 GMT
+source /nfs_eda_sw/softwares/Raptor/instl_dir/07_07_2024_09_15_01/share/raptor/etc/init/flow.tcl
