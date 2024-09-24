d67799bf83fd086ed894ecb91dd1cfbf0e799dc1
alaindargelas <alain.dargelas@gmail.com>
2024-09-24 15:46:52 -0700

    Add verification step

--- EDA-2546/raptor_tcl.tcl
+++ EDA-2546/raptor_tcl.tcl
@@ -1,5 +1,5 @@
 create_design axil_interconnect_wrapper
-target_device GEMINI_COMPACT_104x68
+target_device 1VG28
 add_include_path ./rtl
 add_library_path ./rtl
 add_library_ext .v .sv
@@ -7,3 +7,13 @@ add_design_file ./rtl/axil_interconnect_wrapper_v1_0.v
 set_top_module axil_interconnect_wrapper
 analyze
 synthesize delay
+setup_lec_sim
+simulate icarus gate
+
+packing
+place
+route
+simulate icarus pnr
+sta
+power
+bitstream
